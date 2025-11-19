4c6d2399a9fc11175cf4bc7d50e4415ece1035ed
DanielPuertaM <danielpuerta0926@gmail.com>
2025-11-19 12:26:48 -0500

    Exceptions

---
+++ passenger-service/src/main/java/com/bloque3/passenger_service/exceptions/ApiException.java
@@ -0,0 +1,12 @@
+package com.bloque3.passenger_service.exceptions;
+
+public class ApiException extends RuntimeException{
+
+    public ApiException(String message) {
+        super(message);
+    }
+
+    public ApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
