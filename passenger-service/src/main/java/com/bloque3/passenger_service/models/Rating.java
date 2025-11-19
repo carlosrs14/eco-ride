898dbd20bd27c4e0add951414d45b48b0876909e
DanielPuertaM <danielpuerta0926@gmail.com>
2025-11-19 12:22:23 -0500

    Models

---
+++ passenger-service/src/main/java/com/bloque3/passenger_service/models/Rating.java
@@ -0,0 +1,32 @@
+package com.bloque3.passenger_service.models;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Table("ratings")
+public class Rating {
+
+    @Id
+    private UUID id;
+
+    @Column("trip_id")
+    private UUID tripId;
+    
+    @Column("from_id")
+    private UUID fromId;
+
+    @Column("to_id")
+    private UUID toId;
+
+    private Float score;
+    private String comment;
+    
+}
