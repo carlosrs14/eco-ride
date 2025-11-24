a1b5929d5d583da774571c7c7aff34fe8a8d4f04
rossi <jimenezrossimar2005@gmail.com>
2025-11-24 00:16:16 -0500

    exceptions

---
+++ notification-service/src/main/java/com/bloque3/notification_service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.bloque3.notification_service.exceptions;
+
+public class ResourceNotFoundException extends ApiException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
+    }
+    
+}
