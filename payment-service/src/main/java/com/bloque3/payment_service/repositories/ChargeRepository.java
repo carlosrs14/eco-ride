a7f246b0875e3ff9ac5054d6a2cd7533c91aedfb
DanielPuertaM <danielpuerta0926@gmail.com>
2025-11-25 12:08:34 -0500

    charge repository

--- payment-service/src/main/java/com/bloque3/payment_service/repositories/ChargeRepository.java
+++ payment-service/src/main/java/com/bloque3/payment_service/repositories/ChargeRepository.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Repository;
 
 import com.bloque3.payment_service.models.Charge;
 
+import reactor.core.publisher.Mono;
+
 @Repository
 public interface ChargeRepository extends ReactiveCrudRepository<Charge, UUID> {
+
+    Mono<Charge> findByIdAndIsActiveTrue(UUID id);
 
 }
