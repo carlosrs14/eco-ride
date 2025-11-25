bf831cf76590ad6621d55e20a0d449c18211632c
DanielPuertaM <danielpuerta0926@gmail.com>
2025-11-25 17:06:09 -0500

    openfeing anotation

--- payment-service/src/main/java/com/bloque3/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/bloque3/payment_service/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.bloque3.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.bloque3.payment_service.clients")
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
