ac2d04a80c60ab5316941a1e8e0880ec7f389625
carlosrs14 <crincones578@gmail.com>
2025-11-25 20:47:46 -0500

    adding dependences for test

--- notification-service/src/test/java/com/bloque3/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/bloque3/notification_service/NotificationServiceApplicationTests.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest()
 @ActiveProfiles("test")
 class NotificationServiceApplicationTests {
+	
+    @MockitoBean
+	private JavaMailSender mailSender;
+
 
 	@Test
 	void contextLoads() {
